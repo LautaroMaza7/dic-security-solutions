4bb65fedf61886ec0639d16262616a553e698fdc
Lautaro Maza <lautarodamian33@gmail.com>
2022-07-14 15:46:43 -0300

    contact

--- js/index.js
+++ js/index.js
@@ -10,33 +10,19 @@ function camarasana() {
 function alarmassmart() {
     window.location.href = "../pages/servicios.html";
 }
-  function alarmaspro() {
+function alarmaspro() {
     window.location.href = "../pages/productos-alarmas.html";
 }
-// Owlcarousel
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop:true,
-      margin:10,
-      nav:true,
-      autoplay:true,
-      autoplayTimeout:5000,
-      autoplayHoverPause:true,
-      center: true,
-      navText: [
-          "<i class='fa fa-angle-left'></i>",
-          "<i class='fa fa-angle-right'></i>"
-      ],
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:1
-          },
-          1000:{
-              items:3
-          }
-      }
-    });
-  });
+function dictecno() {
+    window.location.href = "https://lau7aro-maza.github.io/DIC-Tecnology-Solutions/";
+}
+function facebook() {
+    window.location.href = "https://facebook.com/DicSeguridad";
+}
+function instagram() {
+    window.location.href = "https://www.instagram.com/dicsecuritysolutions/";
+}
+function whatsapp() {
+    window.location.href = "https://api.whatsapp.com/send?phone=541164864919";
+}
+
