d34bf6ffbe50a6810397c00b5a99be86dd9c6517
Lautaro Maza <lautarodamian33@gmail.com>
2022-08-01 17:52:49 -0300

    navegacion

--- js/menuapp.js
+++ js/menuapp.js
@@ -1,77 +1,27 @@
-/* const $dropdown = $(".dropdown");
-const $dropdownToggle = $(".dropdown-toggle");
-const $dropdownMenu = $(".dropdown-menu");
-const showClass = "show";
-
-$(window).on("load resize", function() {
-  if (this.matchMedia("(min-width: 768px)").matches) {
-    $dropdown.hover(
-      function() {
-        const $this = $(this);
-        $this.addClass(showClass);
-        $this.find($dropdownToggle).attr("aria-expanded", "true");
-        $this.find($dropdownMenu).addClass(showClass);
-      },
-      function() {
-        const $this = $(this);
-        $this.removeClass(showClass);
-        $this.find($dropdownToggle).attr("aria-expanded", "false");
-        $this.find($dropdownMenu).removeClass(showClass);
-      }
-    );
-  } else {
-    $dropdown.off("mouseenter mouseleave");
+ready(function() {
+    new BottomNavigation();
+  //  new ripple();
+  });
+  
+  function BottomNavigation() {
+    call(".bottomNav > [nav-id]", "click", function() {
+      var target = this.getAttribute("nav-id"),
+        btn = $get("[nav-id='"+target+"']"),
+        targetContent = $get("[nav-content='"+target+"']");
+    
+      var divs = $all(".bottomNav > [nav-id]");
+      var contents = $all("[nav-content]");
+      
+      [].forEach.call(divs, function(el) {
+         $removeClass(el, 'active');
+      });
+      
+      [].forEach.call(contents, function(el) { 
+        $removeClass(el, 'active'); 
+      });
+      
+      $addClass(btn, 'active');
+      $addClass(targetContent, 'active');
+    });
   }
-});
- */
-
-
-
-/* (function($bs) {
-    const CLASS_NAME = 'has-child-dropdown-show';
-        $bs.Dropdown.prototype.toggle = function(_orginal) {
-            return function() {
-                document.querySelectorAll('.' + CLASS_NAME).forEach(function(e) {
-                    e.classList.remove(CLASS_NAME);
-                });
-                let dd = this._element.closest('.dropdown').parentNode.closest('.dropdown');
-                for (; dd && dd !== document; dd = dd.parentNode.closest('.dropdown')) {
-                    dd.classList.add(CLASS_NAME);
-                }
-                return _orginal.call(this);
-            }
-        }($bs.Dropdown.prototype.toggle);
-
-        document.querySelectorAll('.dropdown').forEach(function(dd) {
-            dd.addEventListener('hide.bs.dropdown', function(e) {
-                if (this.classList.contains(CLASS_NAME)) {
-                    this.classList.remove(CLASS_NAME);
-                    e.preventDefault();
-                }
-                if(e.clickEvent && e.clickEvent.composedPath().some(el=>el.classList && el.classList.contains('dropdown-toggle'))){
-                    e.preventDefault();
-                }
-                e.stopPropagation(); // do not need pop in multi level mode
-            });
-        });
-
-        // for hover
-        function getDropdown(element) {
-            return $bs.Dropdown.getInstance(element) || new $bs.Dropdown(element);
-        }
-
-        document.querySelectorAll('.dropdown-hover, .dropdown-hover-all .dropdown').forEach(function(dd) {
-            dd.addEventListener('mouseenter', function(e) {
-                let toggle = e.target.querySelector(':scope>[data-bs-toggle="dropdown"]');
-                if (!toggle.classList.contains('show')) {
-                    getDropdown(toggle).toggle();
-                }
-            });
-            dd.addEventListener('mouseleave', function(e) {
-                let toggle = e.target.querySelector(':scope>[data-bs-toggle="dropdown"]');
-                if (toggle.classList.contains('show')) {
-                    getDropdown(toggle).toggle();
-                }
-            });
-        });
-    })(bootstrap); */
+  
