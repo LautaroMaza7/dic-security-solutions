6da1a4d7ee6b90a985a5a6e5a0a32c69d7dfc25d
Lautaro Maza <lautarodamian33@gmail.com>
2022-08-09 19:12:54 -0300

    cams analogicas

--- js/carousel.js
+++ js/carousel.js
@@ -44,23 +44,32 @@ $(document).ready(function(){
         }
     })
 
-    $('#alarmas-dispositivos .owl-carousel').owlCarousel({
+    $(' #cams-analgcs .owl-carousel').owlCarousel({
         loop:true,
-        margin:-100,
-        nav:false,
+        margin:10,
+        nav:true,
         autoplay:true,
         autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        center: true,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
         responsive:{
             0:{
-                items:2.5
+                items:1
             },
             600:{
-                items:4
+                items:3
             },
             1000:{
-                items:5
+                items:4
             }
         }
     })
+    
 
-  });
+});
