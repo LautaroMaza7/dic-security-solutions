4f82d39b1977f7a94a06fb2d473d3d59c6e034c5
Lautaro Maza <lautarodamian33@gmail.com>
2022-08-30 15:20:31 -0300

    edits index and cercos

--- js/carousel.js
+++ js/carousel.js
@@ -4,9 +4,8 @@ $(document).ready(function(){
         loop:true,
       margin:10,
       nav:true,
-      autoplay:true,
-      autoplayTimeout:2000,
-      autoplayHoverPause:true,
+      autoplay:false,
+
       center: true,
       navText: [
           "<i class='fa fa-angle-left'></i>",
